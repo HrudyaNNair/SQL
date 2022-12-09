da0e18ba09ba1af07130a93fa27fc934768e5f77
HrudyaNNair <85451406+HrudyaNNair@users.noreply.github.com>
2022-12-09 15:56:00 +0530

    Update Cursors.sql

--- Cursors.sql
+++ Cursors.sql
@@ -43,3 +43,22 @@ END LOOP;
 CLOSE ECUR;
 END;
 /
+
+-----------------------------------------------------------------------------------------------------------------------
+
+SELECT * FROM INCSAL2;
+
+-----------------------------------------------------------------------------------------------------------------------
+--Repeat exercise 1 by using cursor for loops--
+
+DECLARE
+CURSOR C IS SELECT ID,NAME,BASIC FROM EMPLOYEE WHERE DEPTID=1;
+BEGIN
+FOR EMP_REC IN C
+LOOP
+UPDATE EMPLOYEE SET SALARY=EMP_REC.SALARY+1000 WHERE ID=EMP_REC.ID;
+INSERT INTO INCSAL2
+VALUES(EMP_REC.ID,EMP_REC.NAME,EMP_REC.BASIC,EMP_REC.BASIC+1000);
+END LOOP;
+END;
+/
